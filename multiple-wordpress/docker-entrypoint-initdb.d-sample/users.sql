8bb2489bc7ffa4ad1a8da2b3587fba7d55d2de80
yusuke <yusuke@newsdict.jp>
2017-06-03 03:21:30 +0000

    add sqls

--- multiple-wordpress/docker-entrypoint-initdb.d-sample/users.sql
+++ multiple-wordpress/docker-entrypoint-initdb.d-sample/users.sql
@@ -1,2 +1,3 @@
 grant all on sample1_database.* to user1 identified by 'pass';
 grant all on sample2_database.* to user2 identified by 'pass';
+grant all on sample3_database.* to user3 identified by 'pass';
